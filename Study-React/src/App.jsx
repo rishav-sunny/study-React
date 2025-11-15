47bad72ec52c25ea572c3911dae6f35c682e6afa
Rishav <rishavsunny.in@gmail.com>
2025-11-16 04:05:05 +0530

    started learnig React

---
+++ Study-React/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='Dad'>
+        <h1 id='child1'>Saurav</h1>
+        <h2 id='child2'>Rishav</h2>
+      </div>
+
+      <div id='uncle'>
+        <h1 id=''></h1>
+      </div>
+    
+    </>
+    
+  )
+}
+
+export default App
